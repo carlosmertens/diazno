b1fc0b05505817b046a709b196cc56fc75312e05
Carlos Mertens <mertenscarlos@yahoo.com>
2020-09-08 15:11:05 +0200

    add events grid functionality

--- components/PreviousDates.js
+++ components/PreviousDates.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
 
+const dataEvents = [
+  { date: '20 May 20', event: 'Brotfabrik Berlin' },
+  { date: '20 Jul 20', event: 'The Swag' },
+];
+
 const PreviousDates = () => {
-  const [date, setDate] = useState('20 Dec 19');
-  const [event, setEvent] = useState('Christmas Special @ Y Not');
+  const [events, setEvents] = useState(dataEvents);
+
+  const eventsGrid = events.map((event, index) => {
+    return (
+      <div key={index}>
+        <p>
+          {event.date} - {event.event}
+        </p>
+      </div>
+    );
+  });
 
   return (
     <div className='previous-dates'>
       <h2>Previous dates</h2>
-      <div>
-        <p>
-          {date} - {event}
-        </p>
-      </div>
+      {eventsGrid}
     </div>
   );
 };
--- components/UpcomingDates.js
+++ components/UpcomingDates.js
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
 
+const dataEvents = [
+  { date: '20 Dec 20', event: 'Brotfabrik Berlin' },
+  { date: '20 Jan 21', event: 'The Swag' },
+];
+
 const UpcomingDates = () => {
-  const [date, setDate] = useState('20 Dec 20');
-  const [event, setEvent] = useState('Brotfabrik Berlin');
+  const [events, setEvents] = useState(dataEvents);
+
+  const eventsGrid = events.map((event, index) => {
+    return (
+      <div key={index}>
+        <p>
+          {event.date} - {event.event}
+        </p>
+      </div>
+    );
+  });
   return (
     <div className='upcoming-dates'>
       <h2>Upcoming Dates</h2>
-      <div>
-        <p>
-          {date} - {event}
-        </p>
-      </div>
+      {eventsGrid}
     </div>
   );
 };
